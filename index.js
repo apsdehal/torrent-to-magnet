9c04425b64d949c1e3ba9bf29dddcc56f6293a5b
Amanpreet Singh <apsdehal@gmail.com>
2015-01-23 01:32:39 +0530

    Add main file

---
+++ index.js
@@ -0,0 +1,50 @@
+var request = require('request');
+var parseTorrent = require('parse-torrent');
+var zlib = require('zlib');
+var extend = require('extend');
+
+module.exports = function (url, options, callback) {
+  // Check if callback is passed
+  if (!callback) {
+    callback = options;
+    options = {};
+  }
+
+  // Make sure callback is async no matter what
+  var asyncCallback = function(err, result, data) {
+    process.nextTick(function() {
+      callback(err, result, data);
+    });
+  };
+
+  var onResponse = function(err, response) {
+    if (err) {
+      return asyncCallback(err);
+    }
+    if (response.statusCode >= 400) {
+      return asyncCallback(new Error('Bad Response: ' + response.statusCode));
+    }
+    if (response.headers['content-encoding'] === 'gzip') {
+      return zlib.gunzip(response.body, onData);
+    }
+    onData(null, response.body);
+  };
+
+  var onData = function(err, data) {
+    if (err) {
+      return asyncCallback(err);
+    }
+    try {
+      var result = parseTorrent(data);
+      var uri = parseTorrent.toMagnetURI(result);
+    } catch (err) {
+      return asyncCallback(err);
+    }
+    asyncCallback(err, uri);
+  };
+
+	if (/^http?:/.test(url)) {
+		return request(extend({ url: url, encoding: null }, options), onResponse);
+	}
+
+};
