7e1db55d39fbc016ec482ebd09489649b41af017
Amanpreet Singh <apsdehal@gmail.com>
2015-07-13 12:47:12 -0700

    update tests

--- test.js
+++ test.js
@@ -3,16 +3,16 @@ var torrentToMagnet = require('./index.js');
 
 it('should return correct magnet uri', function (done) {
 	torrentToMagnet('http://torcache.net/torrent/C7753112704EA2CFC21352B56CED4D3CEB14F575.torrent?title=[kat.cr]horriblesubs.hi.scoool.seha.girl.07.720p.mkv', {}, function (err, uri) {
-		assert.equal(err, '');
-		assert.equal(uri, '');
+		assert.deepEqual(err, {});
+		assert.deepEqual(uri, '');
 		done();
 	});
 });
 
 it('should return correct magnet uri', function (done) {
 	torrentToMagnet('http://torcache.net/torrent/C7753112704EA2CFC21352B56CED4D3CEB14F575.torrent?title=[kat.cr]horriblesubs.hi.scoool.seha.girl.07.720p.mkv', {}, function (err, uri) {
-		assert.equal(err, '');
-		assert.equal(uri, '');
+		assert.deepEqual(err, {});
+		assert.deepEqual(uri, '');
 		done();
 	});
 });
